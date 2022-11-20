28c11d7461da1f58b0c719af6b34fe3f96ae2c1d
Chin Huang <pukkaone@gmail.com>
2022-11-20 11:09:07 -0800

    Implement schema registry

---
+++ schema-registry-server/src/main/java/com/github/pukkaone/accession/schema/registry/controller/RootController.java
@@ -0,0 +1,23 @@
+package com.github.pukkaone.accession.schema.registry.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Endpoint that does nothing and can be used to check for liveness.
+ */
+@RequestMapping(value = "/", produces = MediaTypes.APPLICATION_SCHEMA_REGISTRY)
+@RestController
+public class RootController {
+
+  /**
+   * Gets empty object.
+   *
+   * @return object
+   */
+  @RequestMapping(method = RequestMethod.GET)
+  public String get() {
+    return "{}";
+  }
+}
